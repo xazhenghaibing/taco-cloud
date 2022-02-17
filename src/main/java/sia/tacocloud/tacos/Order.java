2cfcdd04d3c425e60153deafa67afe18544c9670
xazhenghaibing <xazhb85@sohu.com>
2022-02-17 18:01:55 +0800

    init

--- src/main/java/sia/tacocloud/tacos/Order.java
+++ src/main/java/sia/tacocloud/tacos/Order.java
@@ -2,9 +2,13 @@ package sia.tacocloud.tacos;
 
 import lombok.Data;
 import javax.validation.constraints.NotBlank;
+import java.util.Date;
 
 @Data
 public class Order {
+
+  private long id;
+  private Date placedAt;
   @NotBlank(message = "ccCVV is required")
   private String ccCVV;
 
--- src/main/java/sia/tacocloud/tacos/Taco.java
+++ src/main/java/sia/tacocloud/tacos/Taco.java
@@ -2,10 +2,14 @@ package sia.tacocloud.tacos;
 
 import lombok.Data;
 
+import java.util.Date;
 import java.util.List;
 
 @Data
 public class Taco {
+
+  private long id;
+  private Date createdAt;
   private String name;
   private List<String> ingredients;
 }
